082ed3d0e584d98de019984ed945fa469ffaee2f
yisslu <lugohalo999@gmail.com>
2024-09-21 13:36:17 -0600

    Initial Commit

---
+++ ConstraintsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConstraintsPractice
+//
+//  Created by Diplomado on 21/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
